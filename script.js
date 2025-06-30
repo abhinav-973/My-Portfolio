6df135a2fbe0743be7567aa00862e6c9e39ef311
abhinav-973 <vabhinav898@gmail.com>
2025-06-30 12:33:08 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// Download Resume
+document.getElementById("downloadResumeBtn").addEventListener("click", function () {
+    window.location.href = "./assets/abhinav.pdf"; // adjust path if needed
+  });
+
+// Visit GitHub
+document
+  .getElementById("visitGithubBtn")
+  .addEventListener("click", function () {
+    window.open("https://github.com/abhinav-973", "_blank");
+  });
